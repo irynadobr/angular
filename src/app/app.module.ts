e798d4eb6f1df7c930a6324a78d4fb9a2756a218
irynadobr <irenkad@ukr.net>
2021-05-31 15:12:42 +0300

    Lesson1- posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
- import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
- import {HttpClientModule} from "@angular/common/http";
-import { UserComponent } from './user/user.component';
+import { PostsComponent } from './posts/posts.component';
+import {HttpClientModule} from "@angular/common/http";
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    UserComponent
+    PostsComponent,
+    PostComponent
   ],
+  providers: [],
   imports: [
     BrowserModule,
     HttpClientModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
