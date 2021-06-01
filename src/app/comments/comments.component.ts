2a6fead6273a690d911b04db0a4c779edc92a977
irynadobr <irenkad@ukr.net>
2021-06-01 13:46:13 +0300

    Lesson1- postComments

---
+++ src/app/comments/comments.component.ts
@@ -0,0 +1,24 @@
+import { Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Comment } from '../models/Comment';
+import { CommentService } from '../services/comment.service';
+
+@Component({
+  selector: 'app-comments',
+  templateUrl: './comments.component.html',
+  styleUrls: ['./comments.component.css']
+})
+export class CommentsComponent implements OnInit {
+  @Input ()
+  id:number;
+ comments:Comment[];
+  constructor(private commentService:CommentService) {
+
+  }
+
+  ngOnInit(): void {
+
+    this.commentService.getCommentsPost(this.id).subscribe(value => this.comments = value);
+  }
+
+}
