7114e123fba03f881f7cfde82796399f6c1040a8
feden2906 <feden2906@gmail.com>
2021-06-01 19:21:35 +0300

    fix

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,18 +1,17 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Post } from '../models/Post';
+import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+import {Post} from '../models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
-private url = 'https://jsonplaceholder.typicode.com/posts';
-  constructor(private httpClient:HttpClient) {
+  private url = 'https://jsonplaceholder.typicode.com/posts';
 
-  }
-  getPosts():Observable<Post []> {
+  constructor(private httpClient: HttpClient) { }
+
+  getPosts(): Observable<Post []> {
     return this.httpClient.get<Post []>(this.url)
-}
-
+  }
 }
