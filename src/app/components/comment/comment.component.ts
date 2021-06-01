4720280ab72199d48c94586f0bef00891dc24603
feden2906 <feden2906@gmail.com>
2021-06-01 19:21:35 +0300

    fix

---
+++ src/app/components/comment/comment.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+
+import {Comment} from '../../models';
+
+@Component({
+  selector: 'app-comment',
+  templateUrl: './comment.component.html',
+  styleUrls: ['./comment.component.css']
+})
+export class CommentComponent {
+  @Input()
+  comment: Comment
+}
