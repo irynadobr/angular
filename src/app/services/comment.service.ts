988545be0a7becad8702efefb2b8c687cce04b1e
irynadobr <irenkad@ukr.net>
2021-06-01 13:46:13 +0300

    Lesson1- postComments

---
+++ src/app/services/comment.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Comment } from '../models/Comment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+private url ='https://jsonplaceholder.typicode.com/comments';
+
+  constructor(private httpClient:HttpClient) {
+
+  }
+  getComments():Observable<Comment[]>  {
+return this.httpClient.get<Comment[]>(this.url);
+  }
+  getCommentsPost (id: number):Observable<Comment[]> {
+  return this.httpClient.get<Comment[]>(`https://jsonplaceholder.typicode.com/comments?postId=${id}`);
+  }
+}
+
